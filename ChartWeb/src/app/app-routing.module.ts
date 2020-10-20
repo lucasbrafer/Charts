ebe3392258a9dd2bb0f3fd1f7f78438935842e52
Lucas Braga Ferreira <42786572+lucasbrafer@users.noreply.github.com>
2020-10-19 23:16:52 -0300

    add - routes and bootstrap

--- ChartWeb/src/app/app-routing.module.ts
+++ ChartWeb/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'chart', component: ChartsComponent },
+  { path: '',   redirectTo: '/chart', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
